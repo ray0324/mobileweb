adb63dfac8b038a6f7fbd3966caf0c10405feb46
ray0324 <ray0324@foxmail.com>
2016-08-10 00:51:51 +0800

    Signed-off-by: ray0324 <ray0324@foxmail.com>

---
+++ gulpfile.js
@@ -0,0 +1,73 @@
+var gulp         = require('gulp');
+var less         = require('gulp-less');
+var minifycss    = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
+var rename       = require('gulp-rename');
+var plumber      = require('gulp-plumber');
+var sourcemaps   = require('gulp-sourcemaps');
+var uglify       = require('gulp-uglify');
+var notify       = require('gulp-notify');
+var Browsersync  = require('browser-sync').create();
+
+//构建less
+gulp.task('build-less',function(){
+    return gulp.src('./src/less/*.less')
+        .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
+	    .pipe(less())
+    	.pipe(autoprefixer({browsers: ['last 2 versions'],cascade: false}))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifycss())
+		.pipe(gulp.dest('./dist/css/'))
+})
+
+// 构建js
+gulp.task('build-js',function(){
+    return gulp.src(['./src/js/**/*.js','!./src/js/lib/**/*.js'])
+         .pipe(sourcemaps.init())
+         .pipe(uglify({
+             mangle: {except: ['require' ,'exports' ,'module' ,'$']} //保留的变量名称
+         }))
+         .pipe(sourcemaps.write('./maps/'))
+         .pipe(gulp.dest('./dist/js/'))
+})
+
+//移动html
+gulp.task('build-html',function(){
+    return gulp.src('./src/*.html')
+        .pipe(gulp.dest('./dist/'))
+})
+
+//移动静态资源
+gulp.task('copy',function(){
+    gulp.src(['./src/images/**/*'])
+            .pipe(gulp.dest('./dist/images/'));
+    gulp.src(['./src/js/lib/**/*.js'])
+            .pipe(gulp.dest('./dist/js/lib/'));
+})
+
+
+
+//启动一个服务器
+gulp.task('browser-sync', function() {
+    Browsersync.init({
+        open: false,
+        port: 4000,
+        server: {
+            baseDir: "./dist/",
+            directory: true
+        }
+    });
+
+    gulp.watch(['*.html', 'css/*.css', 'js/**/*.js','images/**/*'], {cwd: 'dist'}, function(){
+        Browsersync.reload();
+    });
+
+});
+
+
+
+gulp.task('watch',['browser-sync','copy'],function(){
+    gulp.watch('./src/less/*.less', ['build-less']);
+	gulp.watch('./src/*.html', ['build-html']);
+	gulp.watch('./src/js/*.js', ['build-js']);
+})
---
+++ src/js/app.js
@@ -0,0 +1,49 @@
+require.config({
+    paths: {
+        "jquery": "js/lib/jquery-3.1.0.min",
+        "fastclick": "js/lib/fastclick"
+    }
+})
+
+require(['jquery','fastclick'], function ($,FastClick){
+    FastClick.attach(document.body);
+
+    //导航条功能代码
+    !function(){
+        var $sidebar = $('.sidebar');
+        var $toolbar = $('.toolbar');
+        var $nav     = $('.nav');
+
+        $toolbar.on('click',function(e){
+            e.stopPropagation();
+
+            if(!$(this).hasClass('active')){
+                $(this).addClass('active');
+                $sidebar.show();
+                setTimeout(function(){$sidebar.addClass('active')},0)
+            }else{
+                $(this).removeClass('active');
+                $sidebar.removeClass('active');
+                setTimeout(function(){$sidebar.hide()},350)
+            }
+
+        })
+
+        $nav.on('click',function(e){
+            e.stopPropagation();
+        })
+        
+        $sidebar.on('click',function(e){
+            e.stopPropagation();
+            $toolbar.removeClass('active');
+            $(this).removeClass('active');
+            setTimeout(function(){$(this).hide()}.bind(this),350)
+        })
+    }()
+
+
+
+
+
+    // alert(1)
+});
